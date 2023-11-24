f4583b735d041278f801c4c964cf7216e57e450e
vivek kuamar <vivekkumar474849@gmail.com>
2023-11-24 21:13:37 +0530

    this is calculator

---
+++ logic.js
@@ -0,0 +1,26 @@
+let input=document.querySelector("input");
+let button=document.querySelectorAll("button");
+
+let string=" ";
+button.forEach(element=>{
+    element.addEventListener("touchstart",(e)=>{
+        // console.log(" button was clicked");
+        if(e.target.innerText=='='){
+            string=String(eval(string));
+            input.value=string;
+        }
+        else if(e.target.innerText=="AC"){
+string="";
+input.value=string;
+        }
+        else if(e.target.innerText=="DEL"){
+            string=string.substring(0,string.length-1);
+            input.value=string;
+        }
+        else{
+            string+=e.target.innerText;
+            input.value=string;
+        }
+       
+    })
+})
